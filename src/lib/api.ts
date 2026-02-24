56887d2faa5e249c281524c4a760573d9b0ad7b9
rudney5000 <rudneyngueko@gmail.com>
2026-02-24 03:09:41 +0300

    feat: add api

---
+++ src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios from "axios"
+import { ProductFormData } from "./schema"
+
+const TOKEN = "af1874616430e04cfd4bce30035789907e899fc7c3a1a4bb27254828ff304a77"
+const BASE_URL = "https://app.tablecrm.com/api/v1"
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    token: TOKEN,
+  },
+})
+
+export const getProducts = async (): Promise<ProductFormData[]> => {
+  const response = await api.get("/nomenclature/")
+  return response.data
+}
+
+export const createProduct = async (data: ProductFormData): Promise<ProductFormData> => {
+  const response = await api.post("/nomenclature/", data)
+  return response.data
+}
